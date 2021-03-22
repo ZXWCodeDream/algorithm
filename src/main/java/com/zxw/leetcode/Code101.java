fbcc48e6eca95fe7efededc157488b400fe4177d
zhengxingwang <13384307461@163.com>
2021-03-22 11:57:46 +0800

    LeetCode101 镜像二叉树 非递归实现（队列）

--- src/main/java/com/zxw/leetcode/Code101.java
+++ src/main/java/com/zxw/leetcode/Code101.java
@@ -3,6 +3,9 @@ package main.java.com.zxw.leetcode;
 import main.java.com.zxw.bean.TreeNode;
 import main.java.com.zxw.common.TreeUtil;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * ClassName: Code101
  * Description:
@@ -60,10 +63,48 @@ public class Code101 {
         return circleTree(left.right,right.left) && circleTree(left.left,right.right);
     }
 
+
+    /**
+     * 不使用递归实现
+     * 使用队列演示递归
+     * @param root
+     */
+
+    public static boolean isSymmetric1(TreeNode root){
+
+            return circleTreeOfQueue(root.left,root.right);
+    }
+
+    public static boolean circleTreeOfQueue(TreeNode left,TreeNode right){
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(left);
+        queue.add(right);
+        while (!queue.isEmpty()){
+            //队列两两节点成对比较
+            TreeNode l = queue.poll();
+            TreeNode r = queue.poll();
+            if (l == null && r == null){
+                continue;
+            }
+            if (l == null || r == null || l.val != r.val){
+                return false;
+            }
+
+            // 左子树的左节点和右子树的右节点放入队列比较
+            queue.add(l.left);
+            queue.add(r.right);
+            // 左子树的右节点和右子树的左节点放入队列比较
+            queue.add(l.right);
+            queue.add(r.left);
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        Integer[] arr = {1};
+        Integer[] arr = {9,-42,-42,null,76,76,null,null,13,null,13};
         TreeNode root = TreeUtil.createBinaryTree(arr, 0, arr.length);
-        System.out.println(isSymmetric(root));
+        System.out.println(isSymmetric1(root));
     }
 }
