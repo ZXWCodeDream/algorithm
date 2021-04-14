d6b53d25aceacc07fc7fe47edbceda739ce28909
zhengxingwang <13384307461@163.com>
2021-04-14 09:48:17 +0800

    LeetCode821 最短距离 数组

---
+++ src/main/java/com/zxw/leetcode/Code821.java
@@ -0,0 +1,79 @@
+package main.java.com.zxw.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName: Code821
+ * Description:
+ * 821. 字符的最短距离
+ * 给你一个字符串 s 和一个字符 c ，且 c 是 s 中出现过的字符。
+ *
+ * 返回一个整数数组 answer ，其中 answer.length == s.length 且 answer[i] 是 s 中从下标 i 到离它 最近 的字符 c 的 距离 。
+ *
+ * 两个下标 i 和 j 之间的 距离 为 abs(i - j) ，其中 abs 是绝对值函数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "loveleetcode", c = "e"
+ * 输出：[3,2,1,0,1,0,0,1,2,2,1,0]
+ * 解释：字符 'e' 出现在下标 3、5、6 和 11 处（下标从 0 开始计数）。
+ * 距下标 0 最近的 'e' 出现在下标 3 ，所以距离为 abs(0 - 3) = 3 。
+ * 距下标 1 最近的 'e' 出现在下标 3 ，所以距离为 abs(1 - 3) = 3 。
+ * 对于下标 4 ，出现在下标 3 和下标 5 处的 'e' 都离它最近，但距离是一样的 abs(4 - 3) == abs(4 - 5) = 1 。
+ * 距下标 8 最近的 'e' 出现在下标 6 ，所以距离为 abs(8 - 6) = 2 。
+ * 示例 2：
+ *
+ * 输入：s = "aaab", c = "b"
+ * 输出：[3,2,1,0]
+ *
+ *
+ * 提示：
+ * 1 <= s.length <= 104
+ * s[i] 和 c 均为小写英文字母
+ * 题目数据保证 c 在 s 中至少出现一次
+ * @author zxw
+ * @date 2021/4/10 4:02 下午
+ * @since JDK 1.8
+ */
+public class Code821 {
+
+    /**
+     * 寻找与指定目标最近距离的只需考虑离它最近的左右两边的这两个点：
+     * @param s
+     * @param c
+     * @return
+     */
+    public int[] shortestToChar(String s, char c) {
+
+        int len = s.length();
+        int[] result = new int[len];
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < len; i++){
+            if (s.charAt(i) == c){
+                list.add(i);
+            }
+        }
+        //考虑临界点
+        for (int i = 0; i < len; i++){
+            for (int j = 0; j < list.size();j++){
+                if (i <= list.get(j)){
+                    //此时在改下标位置之前没有指定目标元素出现，那就取指定目标元素与该下标之间的距离
+                    if (j == 0){
+                        result[i] = list.get(j)-i;
+                    }else{
+                        result[i] = Math.min(list.get(j)-i,i-list.get(j-1));
+                    }
+                    break;
+                }
+                //此时在该下标之后就没有指定目标元素出现，那就取该下标与前一个元素之间的距离
+                if (j == list.size()-1){
+                    result[i] = i-list.get(j);
+                }
+            }
+        }
+        return result;
+    }
+}
